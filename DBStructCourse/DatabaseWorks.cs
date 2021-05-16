b71a7a35d1467fa2a8b41424d9b97db31e2ba96a
БЕЗДОТНая Хоопс ✶ ДоскаИгры <vnifedow@bk.ru>
2021-05-16 17:44:49 +0500

    +

---
+++ DBStructCourse/DatabaseWorks.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DBStructCourse
+{
+    public class DatabaseWorks : IDisposable
+    {
+
+        // Реализация интерфейса IDisposable
+
+        private System.ComponentModel.Component components = new System.ComponentModel.Component();
+        bool disposed = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    components.Dispose();
+                }
+                disposed = true;
+            }
+        }
+
+        // Реализация коструктора/деструктора модуля
+
+        SqlConnection connection;
+
+        public DatabaseWorks(string Credentials)
+        {
+            connection = new SqlConnection(Credentials);
+        }
+
+        ~DatabaseWorks()
+        {
+            Dispose(false);
+        }
+
+        // Реализация работы с базой
+        // Вывод таблицы
+
+        public DataSet ReturnTable(string TableName)
+        {
+            SqlDataAdapter sqlData = new SqlDataAdapter($"SELECT * FROM {TableName}", connection);
+            DataSet dataSet = new DataSet();
+            sqlData.Fill(dataSet);
+            return dataSet;
+        }
+
+        // Справочники
+
+        public string AddPhonesData(string PhoneType, string Number) // Tab::(Db_Phones), Fields::(Тип_Телефона, Номер)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand($"INSERT INTO Db_Phones (Тип_Телефона, Номер) VALUES ('{PhoneType}', '{Number}')", connection);
+                return $"Команда выполнена. Задействовано строк таблицы: {command.ExecuteNonQuery()}";
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+
+        public void AddEventType(string EventType) // Tab::(Db_EventType), Fields::(Тип_Мероприятия)
+        {
+
+        }
+
+        public void AddConstructType(string ConstructType) // Tab::(Db_ConstructType), Fields::(Тип_Сооруж)
+        {
+
+        }
+        
+        public void AddLocaleType(string LocaleType) // Tab::(Db_LocaleType), Fields::(ТипНасПункт)
+        {
+
+        }
+
+    }
+}
---
+++ DBStructCourse/MainFrame.Designer.cs
@@ -0,0 +1,317 @@
+﻿
+namespace DBStructCourse
+{
+    partial class MainFrame
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridViewDir = new System.Windows.Forms.DataGridView();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.tabPage8 = new System.Windows.Forms.TabPage();
+            this.tabPage9 = new System.Windows.Forms.TabPage();
+            this.tabPage10 = new System.Windows.Forms.TabPage();
+            this.textBoxDirPhoneType = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxDirPhoneNum = new System.Windows.Forms.TextBox();
+            this.buttonAddDir = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxDirLocaleType = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.tabPage6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDir)).BeginInit();
+            this.tabControl2.SuspendLayout();
+            this.tabPage7.SuspendLayout();
+            this.tabPage8.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1121, 647);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1113, 621);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Населенный пункт";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1087, 596);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Областная организация";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(1113, 621);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Спортивное сооружение";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1113, 621);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Мероприятие";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.buttonAddDir);
+            this.tabPage5.Controls.Add(this.tabControl2);
+            this.tabPage5.Controls.Add(this.dataGridViewDir);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(1113, 621);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Справочник";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.listBox1);
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(1113, 621);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Лог";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(8, 6);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1095, 602);
+            this.listBox1.TabIndex = 0;
+            // 
+            // dataGridViewDir
+            // 
+            this.dataGridViewDir.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDir.Location = new System.Drawing.Point(381, 6);
+            this.dataGridViewDir.Name = "dataGridViewDir";
+            this.dataGridViewDir.Size = new System.Drawing.Size(726, 606);
+            this.dataGridViewDir.TabIndex = 0;
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage7);
+            this.tabControl2.Controls.Add(this.tabPage8);
+            this.tabControl2.Controls.Add(this.tabPage9);
+            this.tabControl2.Controls.Add(this.tabPage10);
+            this.tabControl2.Location = new System.Drawing.Point(3, 6);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(372, 576);
+            this.tabControl2.TabIndex = 1;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Controls.Add(this.label2);
+            this.tabPage7.Controls.Add(this.textBoxDirPhoneNum);
+            this.tabPage7.Controls.Add(this.label1);
+            this.tabPage7.Controls.Add(this.textBoxDirPhoneType);
+            this.tabPage7.Location = new System.Drawing.Point(4, 22);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage7.Size = new System.Drawing.Size(364, 550);
+            this.tabPage7.TabIndex = 0;
+            this.tabPage7.Text = "Телефон";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // tabPage8
+            // 
+            this.tabPage8.Controls.Add(this.label3);
+            this.tabPage8.Controls.Add(this.textBoxDirLocaleType);
+            this.tabPage8.Location = new System.Drawing.Point(4, 22);
+            this.tabPage8.Name = "tabPage8";
+            this.tabPage8.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage8.Size = new System.Drawing.Size(364, 550);
+            this.tabPage8.TabIndex = 1;
+            this.tabPage8.Text = "Тип населенного пункта";
+            this.tabPage8.UseVisualStyleBackColor = true;
+            // 
+            // tabPage9
+            // 
+            this.tabPage9.Location = new System.Drawing.Point(4, 22);
+            this.tabPage9.Name = "tabPage9";
+            this.tabPage9.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage9.Size = new System.Drawing.Size(364, 580);
+            this.tabPage9.TabIndex = 2;
+            this.tabPage9.Text = "Тип сооружения";
+            this.tabPage9.UseVisualStyleBackColor = true;
+            // 
+            // tabPage10
+            // 
+            this.tabPage10.Location = new System.Drawing.Point(4, 22);
+            this.tabPage10.Name = "tabPage10";
+            this.tabPage10.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage10.Size = new System.Drawing.Size(364, 580);
+            this.tabPage10.TabIndex = 3;
+            this.tabPage10.Text = "Тип события";
+            this.tabPage10.UseVisualStyleBackColor = true;
+            // 
+            // textBoxDirPhoneType
+            // 
+            this.textBoxDirPhoneType.Location = new System.Drawing.Point(6, 19);
+            this.textBoxDirPhoneType.Name = "textBoxDirPhoneType";
+            this.textBoxDirPhoneType.Size = new System.Drawing.Size(352, 20);
+            this.textBoxDirPhoneType.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Тип телефона";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Номер";
+            // 
+            // textBoxDirPhoneNum
+            // 
+            this.textBoxDirPhoneNum.Location = new System.Drawing.Point(6, 58);
+            this.textBoxDirPhoneNum.Name = "textBoxDirPhoneNum";
+            this.textBoxDirPhoneNum.Size = new System.Drawing.Size(352, 20);
+            this.textBoxDirPhoneNum.TabIndex = 2;
+            // 
+            // buttonAddDir
+            // 
+            this.buttonAddDir.Location = new System.Drawing.Point(3, 588);
+            this.buttonAddDir.Name = "buttonAddDir";
+            this.buttonAddDir.Size = new System.Drawing.Size(372, 23);
+            this.buttonAddDir.TabIndex = 2;
+            this.buttonAddDir.Text = "Добавить";
+            this.buttonAddDir.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Тип";
+            // 
+            // textBoxDirLocaleType
+            // 
+            this.textBoxDirLocaleType.Location = new System.Drawing.Point(6, 19);
+            this.textBoxDirLocaleType.Name = "textBoxDirLocaleType";
+            this.textBoxDirLocaleType.Size = new System.Drawing.Size(352, 20);
+            this.textBoxDirLocaleType.TabIndex = 4;
+            // 
+            // MainFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1119, 646);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "MainFrame";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDir)).EndInit();
+            this.tabControl2.ResumeLayout(false);
+            this.tabPage7.ResumeLayout(false);
+            this.tabPage7.PerformLayout();
+            this.tabPage8.ResumeLayout(false);
+            this.tabPage8.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.DataGridView dataGridViewDir;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.TabPage tabPage8;
+        private System.Windows.Forms.TabPage tabPage9;
+        private System.Windows.Forms.TabPage tabPage10;
+        private System.Windows.Forms.Button buttonAddDir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxDirPhoneNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxDirPhoneType;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxDirLocaleType;
+    }
+}
+
